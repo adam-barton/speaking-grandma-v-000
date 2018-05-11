f0729a64a9ea1ed6ddaea4b26baca2bd12d66af0
Adam Barton <adam.barton333@gmail.com>
2018-05-11 20:39:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(argument)
   if argument == ("I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   else 
-     "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
   if argument != ("I LOVE YOU GRANDMA!") && argument.upcase
     return "NO, NOT SINCE 1938!"
