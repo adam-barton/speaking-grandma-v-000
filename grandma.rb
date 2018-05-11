3dfdfb3aa2339a0ef1e1779d790f5facfc40defa
Adam Barton <adam.barton333@gmail.com>
2018-05-11 21:55:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(argument)
     if argument == argument.upcase()
       return "NO, NOT SINCE 1938!"
     else
-      return 
+      return "HUH?! SPEAK UP, SONNY!"
     end
     
 end
